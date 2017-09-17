d42b8ab4a570dadca4e0292d5b0d1865ab4a6a80
zitongliu <zitongliu7@gmail.com>
2017-09-17 15:55:50 +1000

    completed JS functionality for + - * /

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-console.log('hello world');
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function minus(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1/num2;
+}
+
+
+function mapOperatorStringToFunction(operator) {
+  switch(operator) {
+    case '+':
+      return add;
+    case '-':
+      return minus;
+    case '*':
+      return multiply;
+    case '/':
+      return divide;
+  }
+}
+
+var calculator = {
+  inputs: [3, 2, 3],
+  output: [],
+  requiredOperations: ['-','+'],
+  calculate: function() {
+    var total = this.inputs[0];
+
+    for (var i = 1; i < this.inputs.length; i++) {
+      var currentOperationFunction = mapOperatorStringToFunction(this.requiredOperations[i-1]);
+      total = currentOperationFunction(total, this.inputs[i]);
+    }
+
+    this.output.push(total);
+  }
+}
+
+calculator.calculate();
+console.log(calculator.output);
