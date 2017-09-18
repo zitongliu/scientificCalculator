e6b39f812ce0ea51ee1f752f2e638794e57806c8
zitongliu <zitongliu7@gmail.com>
2017-09-18 20:13:29 +1000

    solved order of precedence issue

--- js/main.js
+++ js/main.js
@@ -38,17 +38,36 @@ var calculator = {
 
     var inputString = this.inputDisplay;
     var patternOnlyOperators = new RegExp(/[+-/*]/g);
-    var patternNotOperators = new RegExp(/[^+-/*]/g)
+    var patternNotOperators = new RegExp(/[^+-/*]/g);
 
     this.inputs = inputString.split(patternOnlyOperators);
+
 
     // convert array of strings to array of numbers
     for (var i = 0; i < this.inputs.length; i++) {
       this.inputs[i] = parseInt(this.inputs[i], 10);
     }
 
+
     this.requiredOperations = inputString.replace(patternNotOperators, '').split('');
 
+    // Solve order of precedence by:
+    // 1. scan through the operators array
+    // 2. when it detects a higher precedence operator, do the calculation and put the result back into the inputs array
+    for (var i = 0; i < this.requiredOperations.length; i++) {
+      if (this.requiredOperations[i] === '*' || this.requiredOperations[i] === '/') {
+        if (this.requiredOperations[i] === '*') {
+          var localSum = this.inputs[i] * this.inputs[i+1];
+        } else if (this.requiredOperations[i] === '/'){
+          var localSum = this.inputs[i] / this.inputs[i+1];
+        }
+        this.inputs[i] = localSum;
+        this.inputs.splice(i+1,1);
+
+        this.requiredOperations.splice(i,1);
+        i -= 1;
+      }
+    }
 
     var total = this.inputs[0];
     for (var i = 1; i < this.inputs.length; i++) {
@@ -71,8 +90,6 @@ var calculator = {
     this.outputDisplay = '';
   }
 }
-
-
 
 window.onload = function() {
   // Select all input buttons
